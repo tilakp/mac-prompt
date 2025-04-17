cc4938964967a45f2c1e22a64e9c4e6dd2233ac7
Tilak Patel <patelt@amazon.com>
2025-04-17 10:35:24 -0700

    Initial Commit

---
+++ mac-prompt/mac_promptApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_promptApp.swift
+//  mac-prompt
+//
+//  Created by Patel, Tilak on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct mac_promptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mac-promptTests/mac_promptTests.swift
@@ -0,0 +1,16 @@
+//
+//  mac_promptTests.swift
+//  mac-promptTests
+//
+//  Created by Patel, Tilak on 4/17/25.
+//
+
+import Testing
+
+struct mac_promptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
